67191d3613277ca118cb2bd26ab9f0531d032812
worldbeater2 <realdudization@gmail.com>
2023-10-27 02:05:40 +0000

    fixed youtube kinda

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
 
 
 
-document.getElementById('play-button').addEventListener('click', function () {
-    this.style.display = 'none';
-    document.getElementById('thumbnail-container').style.display = 'none';
-    document.getElementById('video-iframe').style.display = 'block';
+window.addEventListener('load', function () {
+    document.getElementById('play-button').addEventListener('click', function () {
+      this.style.display = 'none';
+      document.getElementById('thumbnail-container').style.display = 'none';
+      document.getElementById('video-iframe').style.display = 'block';
+    });
   });
