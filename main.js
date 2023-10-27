23492775983b174475392e898fdaa18a0e7d12d1
worldbeater2 <realdudization@gmail.com>
2023-10-27 01:47:18 +0000

    first

---
+++ main.js
@@ -0,0 +1,8 @@
+
+
+
+document.getElementById('play-button').addEventListener('click', function () {
+    this.style.display = 'none';
+    document.getElementById('thumbnail-container').style.display = 'none';
+    document.getElementById('video-iframe').style.display = 'block';
+  });
